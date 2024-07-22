f10c17d2874270af0cb8969c09d6b0de222f2464
emanuelefavero <emafaux89@gmail.com>
2024-07-22 19:49:52 +0200

    remove docker-compose

--- Dockerfile
+++ Dockerfile
@@ -7,11 +7,12 @@ WORKDIR /root
 # Copy all the files from the current directory to the /root directory
 COPY . .
 
-# Update the package list and install vi
-RUN apt-get update && apt-get install -y vim
+# Update the package list and install vi, fswatch, curl
+RUN apt-get update && apt-get install -y vim inotify-tools curl
+
 
 # Set all scripts to be executable
 RUN chmod +x *.sh
 
-# Run bash when the container starts
-CMD ["/bin/bash", "script.sh"]
+# Set the default command to start a shell
+CMD ["/bin/bash"]
--- watch.sh
+++ watch.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Startup message
-echo "File: script.sh"
-echo "Watching for changes..."
+# Path to the script you want to watch
+WATCHED_FILE="./script.sh"
 
-# Watch for changes in the current directory and subdirectories
-fswatch -o ./script.sh | while read; do
-    docker-compose up --build
+# Function to handle file changes
+run_script() {
+    echo "Change detected in $WATCHED_FILE. Running script..."
+    bash "$WATCHED_FILE"
+}
+
+# Monitor the file for changes
+inotifywait -m -e modify "$WATCHED_FILE" | while read; do
+    run_script
 done
