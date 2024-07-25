31bd637333091a95dc1299d40b4f828d5c23fb13
emanuelefavero <emafaux89@gmail.com>
2024-07-25 18:01:24 +0200

    add scripts/variables

---
+++ scripts/variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name="John"
+echo "Hello $name"
+
+# TIP: You can't use spaces around the equal sign when assigning variables
+# TIP: Use double quotes when you want to use variables in strings, single quotes will not interpolate variables
