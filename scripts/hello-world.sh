862dc9ec33a6534af54d60ea9b00e91f5ca7ef6d
emanuelefavero <emafaux89@gmail.com>
2024-07-25 18:00:03 +0200

    add scripts/hello-world

---
+++ scripts/hello-world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello World"
+
+# TIP: Use bash script.sh or sh script.sh to run this script if not executable
+# TIP: Use chmod +x script.sh to make the script executable
+# TIP: After making the script executable, you can run it using ./script.sh
